2f514d36f07327ae3f1e7cc700777f6dc440872c
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-07-01 23:19:24 +0200

    Added test with storybook

---
+++ source/renderer/components/Link/Link.story.tsx
@@ -0,0 +1,33 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { FunctionComponent } from "react";
+import Link from "~renderer/components/Link/Link";
+import LinkProperties from "~renderer/components/Link/Link.types";
+
+export default {
+  title: "Link",
+  component: Link,
+} as ComponentMeta<typeof Link>;
+
+const Template: ComponentStory<FunctionComponent<LinkProperties>> = ({
+  children,
+  ...rest
+}: LinkProperties): JSX.Element => {
+  return <Link {...rest}>{children}</Link>;
+};
+
+export const LinkExample: ComponentStory<typeof Link> = Template.bind({});
+
+LinkExample.args = {
+  children: "Google.com",
+  href: "https://google.com",
+};
+
+export const InsecureLinkExample: ComponentStory<typeof Link> = Template.bind(
+  {}
+);
+
+InsecureLinkExample.args = {
+  children: "Google.com",
+  href: "https://google.com",
+  insecure: true,
+};
