83286e17eaf7d92a5ab10a41b23e1bd687224fb1
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-07-01 23:02:14 +0200

    init

---
+++ source/renderer/components/Link/Link.tsx
@@ -0,0 +1,37 @@
+import { Link as LinkBase } from "react-router-dom";
+import LinkProperties, {
+  LinkPropertiesInternal,
+  LinkPropertiesExternal,
+} from "~renderer/components/Link/Link.types";
+
+const Link = ({
+  external,
+  children,
+  href,
+  rel,
+  insecure,
+  ...rest
+}: LinkProperties) => {
+  const relWithSpace: string = `${rel} `;
+  const fixedRel: string = `${rel ? relWithSpace : ""}${
+    !insecure || "noopener norefferer"
+  }`;
+  return external ? (
+    <a
+      {...(rest as Omit<LinkPropertiesExternal, "rel" | "children">)}
+      rel={fixedRel}
+    >
+      {children}
+    </a>
+  ) : (
+    <LinkBase
+      {...(rest as Omit<LinkPropertiesInternal, "rel" | "to" | "children">)}
+      rel={fixedRel}
+      to={href}
+    >
+      {children}
+    </LinkBase>
+  );
+};
+
+export default Link;
---
+++ source/renderer/components/Link/Link.types.tsx
@@ -0,0 +1,35 @@
+import {
+  AnchorHTMLAttributes,
+  DetailedHTMLProps,
+  PropsWithChildren,
+  RefAttributes,
+} from "react";
+import { LinkProps } from "react-router-dom";
+
+export type LinkPropertiesInternalBase = LinkProps &
+  RefAttributes<HTMLAnchorElement>;
+
+export type LinkPropertiesInternal = Omit<
+  LinkPropertiesInternalBase,
+  "href"
+> & {
+  external?: boolean;
+  href: string;
+};
+
+export type LinkPropertiesExternalBase = DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>;
+
+export type LinkPropertiesExternal = Omit<LinkPropertiesExternalBase, "to"> & {
+  external: true;
+  href: LinkPropertiesInternal["href"];
+};
+
+type LinkProperties = PropsWithChildren<{
+  insecure?: boolean;
+}> &
+  (LinkPropertiesExternal | LinkPropertiesInternal);
+
+export default LinkProperties;
