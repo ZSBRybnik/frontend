6813abe4d67701e0c1695c5e49cd4c7625f3235d
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-07-02 13:39:35 +0200

    Added global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+declare module "duplicate-package-checker-webpack-plugin" {
+  declare class DuplicatePackageCheckerPlugin {}
+  export default DuplicatePackageCheckerPlugin;
+}
+declare module "json-minimizer-webpack-plugin";
+declare module "script-ext-html-webpack-plugin";
+declare module "interpolate-html-plugin";
+declare module "native-addon-rust";
+declare module "robotstxt-webpack-plugin";
+declare module "@vue/preload-webpack-plugin";
