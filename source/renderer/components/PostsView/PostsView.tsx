4feb9ca6c149d38a090dc0d2230c88fd1322b921
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-11-01 13:29:07 +0100

    init

--- source/renderer/components/PostsView/PostsView.tsx
+++ source/renderer/components/PostsView/PostsView.tsx
@@ -1,48 +1,50 @@
 /* eslint-disable max-params */
-import { useHookstate } from "@hookstate/core";
+import useState from "~frontend/source/renderer/hooks/useState/useState";
 import { FunctionComponent, useEffect } from "react";
 import VisibilitySensor from "react-visibility-sensor";
 import { useQuery } from "../../utils/trpc-utilities/trpc-utilities";
 import Post from "../Post/Post";
+import { Post as PostType } from "~backend/node_modules/@prisma/postgresql";
 
 const PostsView: FunctionComponent = () => {
-  const shouldFetchState = useHookstate(true);
-  const postsState = useHookstate(
-    [] as { title: string; id: number; brief: string }[],
-  );
-  const { data } = useQuery(
-    ["getPosts", { range: 10, skip: postsState.length }],
-    {
-      enabled: shouldFetchState.get(),
-      onSuccess: () => {
-        shouldFetchState.set(false);
-      },
+  const { value: shouldFetch, setValue: setShouldFetch } = useState<{
+    value: boolean;
+  }>({ value: true });
+  const { value: posts, setValue: setPosts } = useState<{
+    value: Pick<PostType, "id" | "title" | "brief">[];
+  }>({
+    value: [],
+  });
+  const { data } = useQuery(["getPosts", { range: 10, skip: posts.length }], {
+    enabled: shouldFetch,
+    onSuccess: () => {
+      setShouldFetch(false);
     },
-  );
+  });
   useEffect(() => {
     if (data) {
-      postsState.merge(data);
+      setPosts([...posts, ...data]);
     }
   }, [data]);
   return (
     <div>
-      {postsState.map((post, index) => {
+      {posts.map(({ title, brief, id }, index) => {
         const PostElement = (
           <Post
-            title={post.get().title}
-            brief={post.get().brief}
-            id={post.get().id}
-            key={`post-${post.get().id}`}
+            title={title}
+            brief={brief}
+            id={id}
+            key={`post-${id}`}
             alt="ZSB Rybnik"
             src="https://zsbrybnik.pl/wp-content/uploads/2017/09/logo_zsb_small.png"
           />
         );
-        return postsState.length - 1 === index ? (
+        return posts.length - 1 === index ? (
           <VisibilitySensor
             onChange={(isVisible: boolean) => {
-              shouldFetchState.set(isVisible);
+              setShouldFetch(isVisible);
             }}
-            key={`posts-sensor-${post.get().id}`}
+            key={`posts-sensor-${id}`}
           >
             {PostElement}
           </VisibilitySensor>
