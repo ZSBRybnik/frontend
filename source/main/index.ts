0c047fef2c8d004e0aa9765714d32eae16151b72
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-11-12 16:35:08 +0100

    init

--- source/main/index.ts
+++ source/main/index.ts
@@ -1,12 +1,5 @@
 /* eslint-disable max-params */
-import {
-  app,
-  BrowserWindow,
-  ipcMain,
-  Menu,
-  IpcMainEvent,
-  shell,
-} from "electron";
+import { app, BrowserWindow, ipcMain, Menu, IpcMainEvent } from "electron";
 import { join } from "path";
 import { fromEvent } from "rxjs";
 import "source-map-support/register";
@@ -32,10 +25,6 @@ ipcMain.handle(IpcEvents.GetOperatingSystemInformation, async () => {
 app.on(AppEvents.Ready, (): void => {
   Menu.setApplicationMenu(null);
   mainWindow.content = new BrowserWindow(mainWindow.settings);
-  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
-    shell.openExternal(url);
-    return { action: "deny" };
-  });
   mainWindow.content.loadURL(`http://localhost:${port}/cat`);
   fromEvent(mainWindow.content, BrowserWindowEvent.ReadyToShow).subscribe(
     () => {
