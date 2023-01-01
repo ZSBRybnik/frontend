857460153b9ab88ffedd0c2c2e2ce9b4837e774b
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2023-01-01 19:54:22 +0100

    init

--- source/main/rest/utils/createServer/createServer.ts
+++ source/main/rest/utils/createServer/createServer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-params */
 import type { Express } from "express";
 import express from "express";
 import applyMiddlewares from "~frontend/source/main/rest/utils/applyMiddlewares/applyMiddlewares";
@@ -8,17 +9,26 @@ import type {
   CreateServerOutput,
 } from "~frontend/source/main/rest/utils/createServer/createServer.types";
 import listenOnPort from "~frontend/source/main/rest/utils/listenOnPort/listenOnPort";
+import next from "next";
 
-const createServer: CreateServer = ({
+const createServer: CreateServer = async ({
   port,
-}: CreateServerArguments): CreateServerOutput => {
-  const server: Express = express();
-  applyMiddlewares({ instance: server });
-  applyRoutes({ instance: server });
+}: CreateServerArguments): Promise<CreateServerOutput> => {
+  const nextServer = next({});
+  const handle = nextServer.getRequestHandler();
+  await nextServer.prepare();
+  const backendServer: Express = express();
+  const nextServerExpress: Express = express();
+  nextServerExpress.get("*", (req, res) => {
+    return handle(req, res);
+  });
+  nextServerExpress.listen(2137);
+  applyMiddlewares({ instance: backendServer });
+  applyRoutes({ instance: backendServer });
   const { httpServer, port: outputPort } = listenOnPort({
-    instance: server,
+    instance: backendServer,
     port,
   });
-  return { server, httpServer, port: outputPort };
+  return { server: backendServer, httpServer, port: outputPort };
 };
 export default createServer;
