67b3d56f13a828e4e36624d89422de32f52d4567
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2023-01-08 20:20:38 +0100

    init

--- source/scripts/build/loaders/getTypeScriptLoader/getTypeScriptLoader.ts
+++ source/scripts/build/loaders/getTypeScriptLoader/getTypeScriptLoader.ts
@@ -40,7 +40,6 @@ const getTypeScriptLoader = ({
               }
             : {
                 presets: [
-                  "next/babel",
                   [
                     "@babel/env",
                     {
