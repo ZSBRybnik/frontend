c61e39ef1a9686c772b5936461bb3baa12a1d534
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2023-02-20 08:33:03 +0100

    init

--- source/scripts/runner/commands/postInstall/postInstall.ts
+++ source/scripts/runner/commands/postInstall/postInstall.ts
@@ -11,7 +11,6 @@ import handlePostInstallWindows from "~frontend/source/scripts/runner/utils/hand
     $.prefix = "";
   }
   await $`ionic config set -g npmClient yarn && husky install && yarn config set network-timeout 1000000000 -g && yarn run generate-types`;
-
   if (os === "win32") {
     await handlePostInstallWindows();
   } else if (os === "darwin") {
