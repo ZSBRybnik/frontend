eb5cc8a499e3f672803c86e3d79d88e82670bf7c
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2023-03-23 19:17:09 +0100

    init

--- source/e2e/index.integration.test.ts
+++ source/e2e/index.integration.test.ts
@@ -1,15 +1,22 @@
-import { exec, execSync } from "child_process";
+import { platform } from "os";
+import { $ } from "zx";
 import destination from "~frontend/source/scripts/build/constants/destination/destination";
 import scriptsKeys from "~frontend/source/scripts/build/constants/scriptsKeys/scriptsKeys";
 import Programs from "~frontend/source/scripts/runner/types/programs/programs";
 
 beforeAll((): Promise<void> => {
   return new Promise<void>((resolve): void => {
-    execSync(
-      `cross-env NODE_OPTIONS="--max-old-space-size=8192" ${Programs.Yarn} run ${scriptsKeys["build-web"]}`,
-    );
-    exec(`yarn run ${Programs.Serve} -s ${destination}`);
-    resolve();
+    const os: NodeJS.Platform = platform();
+    if (os === "win32") {
+      $.shell = "cmd";
+      $.prefix = "";
+    }
+    $.verbose = true;
+    (async () => {
+      await $`cross-env NODE_OPTIONS="--max-old-space-size=8192" ${Programs.Yarn} run ${scriptsKeys["build-web"]}`;
+      $`yarn run ${Programs.Serve} -s ${destination}`;
+      resolve();
+    })();
   });
 });
 
