11fe1c550890592d89a56a258b081e29fc20f902
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2023-06-18 19:48:59 +0200

    init
    Co-authored-by: Krzysztof Zawisła <zawislakrzysztof.zk@gmail.com>

--- source/scripts/build/wrappers/getPlugins/getPlugins.ts
+++ source/scripts/build/wrappers/getPlugins/getPlugins.ts
@@ -1,4 +1,3 @@
-import { webpack } from "million/compiler";
 import getBrotliCompressionPlugin from "~frontend/source/scripts/build/plugins/getBrotliCompressionPlugin/getBrotliCompressionPlugin";
 import getBundleAnalyzerPlugin from "~frontend/source/scripts/build/plugins/getBundleAnalyzerPlugin/getBundleAnalyzerPlugin";
 import getCleanWebpackPlugin from "~frontend/source/scripts/build/plugins/getCleanWebpackPlugin/getCleanWebpackPlugin";
@@ -64,7 +63,6 @@ const getPlugins = ({
     getNodePolyfillPlugin(),
     getSitemapPlugin({ extendedMode }),
     getCopyWebpackPlugin({ extendedMode }),
-    webpack(),
   ].filter(Boolean);
 };
 
