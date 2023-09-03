ff7f73be82fa8d2dfbe0eafde407e97e48f222e0
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2023-09-03 22:45:11 +0200

    init
    Co-authored-by: Borys Malinowski <borys-malinowski@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,64 @@
-import reactPlugin from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import legacy from "@vitejs/plugin-legacy";
+import reactPluginBabel from "@vitejs/plugin-react";
+import reactPluginSWC from "@vitejs/plugin-react-swc";
+import { writeFileSync } from "fs";
+import { join } from "path";
 import process from "process/browser";
-import { defineConfig, PluginOption } from "vite";
+import { compileFile } from "pug";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import getTargetVersion from "./source/scripts/build/utils/getTargetVersion/getTargetVersion";
 
-export default defineConfig({
-  plugins: [reactPlugin() as PluginOption, tsconfigPaths()],
-  define: {
-    global: "window",
-    process,
-  },
+export default defineConfig(({ mode }) => {
+  const result = compileFile(
+    join("source", "public", "notStatic", "indexWebVite.pug"),
+    {
+      filename: "indexWebVite.pug",
+      basedir: join("source", "public", "notStatic"),
+    },
+  );
+  const html: string = result();
+  writeFileSync("index.html", html.replaceAll("%PUBLIC_URL%", "public"));
+  return {
+    build: {
+      outDir: "destination",
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true,
+          }),
+        ],
+      },
+    },
+    publicDir: join("source", "public", "static"),
+    plugins: [
+      legacy({
+        targets: getTargetVersion({ targetToModern: false }).split(","),
+      }),
+      mode === "development" ? reactPluginSWC() : reactPluginBabel(),
+      tsconfigPaths(),
+    ],
+    resolve: {
+      alias: {
+        "react/jsx-runtime.js": "react/jsx-runtime",
+        "react/jsx-dev-runtime.js": "react/jsx-dev-runtime",
+        zlib: "zlib",
+        stream: "stream-browserify",
+        Buffer: "buffer",
+        util: "util",
+        process: "process/browser",
+      },
+    },
+    define: {
+      global: "window",
+      process,
+    },
+  };
 });
